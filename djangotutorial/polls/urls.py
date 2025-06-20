a035e0c22b46a725bec4adc80ae9aefa68743400
realtanx <hamniba8@gmail.com>
2025-06-20 11:23:17 +0800

    Create paths for polls app

--- djangotutorial/polls/urls.py
+++ djangotutorial/polls/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index)
+    path("", views.index),
+    path("<int:qs_id>/", views.detail),
+    path("<int:qs_id>/results/", views.results),
+    path("<int:qs_id>/vote/", views.vote),
 ]
--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -5,3 +5,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("the polls index page")
+
+
+def detail(request, qs_id):
+    return HttpResponse("you're looking at question %s" %qs_id)
+
+
+def results(request, qs_id):
+    return HttpResponse("you're looking at the results of question %s" %qs_id)
+
+
+def vote(request, qs_id):
+    return HttpResponse("you're voting on question %s" %qs_id)
